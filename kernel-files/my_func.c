b4884df19435aab6e99960b6ebf88acdc9f38227
andresryes <enriquebolanos@ufm.edu>
2021-03-07 16:25:28 -0600

    rip kernel

--- kernel-files/my_func.c
+++ kernel-files/my_func.c
@@ -1,136 +1,56 @@
-#include <linux/kernel.h>
-#include <linux/slab.h>
-// #include <stdio.h> 
-// #include <stdlib.h> 
-#define bool int 
+#define bool int
+char findClosing(char c)
+{
+	if (c == '(')
+		return ')';
+	if (c == '{')
+		return '}';
+	if (c == '[')
+		return ']';
+	return -1;
+}
 
-// structure of a stack node 
-struct sNode { 
-	char data; 
-	struct sNode* next; 
-}; 
+// function to check if parenthesis are
+// balanced.
+bool check(char *expr, int n)
+{
+	// Base cases
+	if (n == 0)
+		return 1;
+	if (n == 1)
+		return 0;
+	if (expr[0] == ')' || expr[0] == '}' || expr[0] == ']')
+		return 0;
 
-// Function to push an item to stack 
-void push(struct sNode** top_ref, int new_data); 
+	// Search for closing bracket for first
+	// opening bracket.
+	char closing = findClosing(expr[0]);
 
-// Function to pop an item from stack 
-int pop(struct sNode** top_ref); 
+	// count is used to handle cases like
+	// "((()))". We basically need to
+	// consider matching closing bracket.
+	int i, count = 0;
+	for (i = 1; i < n; i++) {
+		if (expr[i] == expr[0])
+			count++;
+		if (expr[i] == closing) {
+			if (count == 0)
+				break;
+			count--;
+		}
+	}
 
-// Returns 1 if character1 and character2 are matching left 
-// and right Brackets 
-bool isMatchingPair(char character1, char character2) 
-{ 
-	if (character1 == '(' && character2 == ')') 
-		return 1; 
-	else if (character1 == '{' && character2 == '}') 
-		return 1; 
-	else if (character1 == '[' && character2 == ']') 
-		return 1; 
-	else
-		return 0; 
-} 
+	// If we did not find a closing
+	// bracket
+	if (i == n)
+		return 0;
 
-// Return 1 if expression has balanced Brackets 
-asmlinkage bool areBracketsBalanced(char *exp) 
-{ 
-	int i = 0; 
+	// If closing bracket was next
+	// to open
+	if (i == 1)
+		return check(expr + 2, n - 2);
 
-	// Declare an empty character stack 
-	struct sNode* stack = NULL; 
-
-	// Traverse the given expression to check matching 
-	// brackets 
-	while (exp[i]) 
-	{ 
-		// If the exp[i] is a starting bracket then push 
-		// it 
-		if (exp[i] == '{' || exp[i] == '(' || exp[i] == '[') 
-			push(&stack, exp[i]); 
-
-		// If exp[i] is an ending bracket then pop from 
-		// stack and check if the popped bracket is a 
-		// matching pair*/ 
-		if (exp[i] == '}' || exp[i] == ')'
-			|| exp[i] == ']') { 
-
-			// If we see an ending bracket without a pair 
-			// then return false 
-			if (stack == NULL) 
-				return 0; 
-
-			// Pop the top element from stack, if it is not 
-			// a pair bracket of character then there is a 
-			// mismatch. 
-			// his happens for expressions like {(}) 
-			else if (!isMatchingPair(pop(&stack), exp[i])) 
-				return 0; 
-		} 
-		i++; 
-	} 
-
-	// If there is something left in expression then there 
-	// is a starting bracket without a closing 
-	// bracket 
-    printk("my_func\n");
-	if (stack == NULL) 
-		return 1; // balanced 
-	else
-		return 0; // not balanced 
-} 
-
-// Driver code 
-// int main() 
-// { 
-// 	char exp[10] = "{5-3)}[2]"; 
-
-// 	// Function call 
-// 	if (areBracketsBalanced(&exp[0])) 
-// 		printf("Balanced \n"); 
-// 	else
-// 		printf("Not Balanced \n"); 
-// 	return 0; 
-// } 
-
-// Function to push an item to stack 
-void push(struct sNode** top_ref, int new_data) 
-{ 
-	// allocate node 
-	struct sNode* new_node 
-		= (struct sNode*)malloc(sizeof(struct sNode)); 
-
-	if (new_node == NULL) { 
-		printf("Stack overflow n"); 
-		getchar(); 
-		exit(0); 
-	} 
-
-	// put in the data 
-	new_node->data = new_data; 
-
-	// link the old list off the new node 
-	new_node->next = (*top_ref); 
-
-	// move the head to point to the new node 
-	(*top_ref) = new_node; 
-} 
-
-// Function to pop an item from stack 
-int pop(struct sNode** top_ref) 
-{ 
-	char res; 
-	struct sNode* top; 
-
-	// If stack is empty then error 
-	if (*top_ref == NULL) { 
-		printf("Stack overflow n"); 
-		getchar(); 
-		exit(0); 
-	} 
-	else { 
-		top = *top_ref; 
-		res = top->data; 
-		*top_ref = top->next; 
-		free(top); 
-		return res; 
-	} 
+	// If closing bracket was somewhere
+	// in middle.
+	return check(expr + 1, i - 1) && check(expr + i + 1, n - i - 1);
 }
