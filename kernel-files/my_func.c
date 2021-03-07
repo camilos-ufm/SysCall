212aaee409d01498e82f40f6c251d197af78212b
andresryes <enriquebolanos@ufm.edu>
2021-03-07 17:29:32 -0600

    rip 3.0

--- kernel-files/my_func.c
+++ kernel-files/my_func.c
@@ -1,35 +1,39 @@
 #include <linux/kernel.h>
 #define bool int
 
+char findClosing(char c)
+{
+	if (c == '(')
+		return ')';
+	if (c == '{')
+		return '}';
+	if (c == '[')
+		return ']';
+	return -1;
+}
+
 // function to check if parenthesis are
 // balanced.
-asmlinkage bool areBracketsBalanced(char *expr, int *n)
+asmlinkage bool areBracketsBalanced(char *expr, int n)
 {
+    printk("my_syscall_1 : %d\n", n);
 	// Base cases
-	if (n[0] == 0)
+	if (n == 0)
 		return 1;
-	if (n[0] == 1)
+	if (n == 1)
 		return 0;
 	if (expr[0] == ')' || expr[0] == '}' || expr[0] == ']')
 		return 0;
 
 	// Search for closing bracket for first
 	// opening bracket.
-    char c = (expr[0]);
-    char closing;
-    if (c == '(')
-		closing = ')';
-	if (c == '{')
-		closing = '}';
-	if (c == '[')
-		closing = ']';
+	char closing = findClosing(expr[0]);
 
 	// count is used to handle cases like
 	// "((()))". We basically need to
 	// consider matching closing bracket.
 	int i, count = 0;
-	for (i = 1; i < n[0]; i++) {
-        printk("my_syscall_1 : %d\n", i);
+	for (i = 1; i < n; i++) {
 		if (expr[i] == expr[0])
 			count++;
 		if (expr[i] == closing) {
@@ -41,16 +45,15 @@ asmlinkage bool areBracketsBalanced(char *expr, int *n)
     printk("My func rip\n");
 	// If we did not find a closing
 	// bracket
-	if (i == n[0])
+	if (i == n)
 		return 0;
 
 	// If closing bracket was next
 	// to open
 	if (i == 1)
-		return areBracketsBalanced(expr[2], n[2]);
+		return areBracketsBalanced(expr + 2, n - 2);
 
 	// If closing bracket was somewhere
 	// in middle.
-    // n[0] = i - 1;
-	// return areBracketsBalanced(expr[1], n[0]) && areBracketsBalanced(expr + i + 1, n - i - 1);
+	return areBracketsBalanced(expr + 1, i - 1) && areBracketsBalanced(expr + i + 1, n - i - 1);
 }
